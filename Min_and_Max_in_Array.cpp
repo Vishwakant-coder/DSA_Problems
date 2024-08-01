61124d77acddd771ffa17c98db4f1554f458dd41
Vishwakant-coder <work.vishwakant.edu@gmail.com>
2024-08-01 23:10:24 +0530

    Add new file

---
+++ Min_and_Max_in_Array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    pair<long long, long long> getMinMax(vector<long long int> arr)
+    {
+        // code here
+        long long mini = arr[0];
+        long long maxi = arr[0];
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (mini > arr[i])
+            {
+                mini = arr[i];
+            }
+            else if (maxi < arr[i])
+            {
+                maxi = arr[i];
+            }
+        }
+        return {mini, maxi};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<long long int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        Solution ob;
+        pair<long long, long long> pp = ob.getMinMax(arr);
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
